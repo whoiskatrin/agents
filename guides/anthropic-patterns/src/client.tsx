9807d2f0b8864d42b04a452a45cd2afb7134dba7
Sunil Pai <spai@cloudflare.com>
2025-02-02 14:09:13 +0000

    hydrateRoot

--- guides/anthropic-patterns/src/client.tsx
+++ guides/anthropic-patterns/src/client.tsx
@@ -1,4 +1,3 @@
-import { createRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import App from "./app";
-const root = createRoot(document.getElementById("app")!);
-root.render(<App />);
+hydrateRoot(document.getElementById("app")!, <App />);
