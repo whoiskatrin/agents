a9248c74c3b7af2a0085d15f02712c243e870cc3
Sunil Pai <spai@cloudflare.com>
2025-03-13 13:34:01 +0000

    add state sync to the regular agent client (#83)
    fixes https://github.com/cloudflare/agents/issues/9

---
+++ .changeset/warm-files-clean.md
@@ -0,0 +1,7 @@
+---
+"agents-sdk": patch
+---
+
+add state sync to the regular agent client
+
+fixes https://github.com/cloudflare/agents/issues/9
--- packages/agents/src/client.ts
+++ packages/agents/src/client.ts
@@ -7,11 +7,16 @@ import {
 /**
  * Options for creating an AgentClient
  */
-export type AgentClientOptions = Omit<PartySocketOptions, "party" | "room"> & {
+export type AgentClientOptions<State = unknown> = Omit<
+  PartySocketOptions,
+  "party" | "room"
+> & {
   /** Name of the agent to connect to */
   agent: string;
   /** Name of the specific Agent instance */
   name?: string;
+  /** Called when the Agent's state is updated */
+  onStateUpdate?: (state: State, source: "server" | "client") => void;
 };
 
 /**
@@ -30,23 +35,28 @@ export type AgentClientFetchOptions = Omit<
 /**
  * WebSocket client for connecting to an Agent
  */
-export class AgentClient extends PartySocket {
-  static fetch(opts: PartyFetchOptions): Promise<Response> {
+export class AgentClient<State = unknown> extends PartySocket {
+  /**
+   * @deprecated Use agentFetch instead
+   */
+  static fetch(_opts: PartyFetchOptions): Promise<Response> {
     throw new Error(
       "AgentClient.fetch is not implemented, use agentFetch instead"
     );
   }
   agent: string;
   name: string;
-  constructor(opts: AgentClientOptions) {
+  #options: AgentClientOptions<State>;
+  constructor(options: AgentClientOptions<State>) {
     super({
       prefix: "agents",
-      party: opts.agent,
-      room: opts.name || "default",
-      ...opts,
+      party: options.agent,
+      room: options.name || "default",
+      ...options,
     });
-    this.agent = opts.agent;
-    this.name = opts.name || "default";
+    this.agent = options.agent;
+    this.name = options.name || "default";
+    this.#options = options;
     // warn if agent or name isn't in lowercase
     if (this.agent !== this.agent.toLowerCase()) {
       console.warn(
@@ -58,6 +68,18 @@ export class AgentClient extends PartySocket {
         `Agent instance name: ${this.name} should probably be in lowercase. Received: ${this.name}`
       );
     }
+    this.addEventListener("message", (event) => {
+      if (typeof event.data === "string") {
+        const parsedMessage = JSON.parse(event.data);
+        if (parsedMessage.type === "cf_agent_state") {
+          this.#options.onStateUpdate?.(parsedMessage.state, "server");
+        }
+      }
+    });
+  }
+  setState(state: State) {
+    this.send(JSON.stringify({ type: "cf_agent_state", state }));
+    this.#options.onStateUpdate?.(state, "client");
   }
 }
 
